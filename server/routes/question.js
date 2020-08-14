e684853add5ec913aac8d42944d1f6726e06428b
Rishikesh Pathak <pathakrishikesh.1999@gmail.com>
2020-08-14 12:30:02 +0530

    getTestcase function added in route

--- server/routes/question.js
+++ server/routes/question.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Question = require('../models/questionModel');
 const route = express.Router();
 
 const {
@@ -25,8 +26,19 @@ route.delete('/:questionId', deleteQuestionById);
 route.patch('/:questionId', patchQuestionById);
 
 // testcase routes
-route.get('/lund-lasun', onlyAdmins, (req, res) => {
-  //yahaan code likh
+route.get('/testcase', onlyAdmins, (req, res) => {
+  try {
+    const question = Question.findOne({
+      _id: req.params.questionId,
+    });
+    res.status(200).json({
+      message: question.testcase[0],
+    });
+  } catch (err) {
+    res.status(401).json({
+      message: err.message,
+    });
+  }
 });
 
 module.exports = route;
