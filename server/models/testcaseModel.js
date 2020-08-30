8c2779c64d30ab79e8720fc521289a6ed2f9a67f
sanithps98 <pssanith@karunya.edu.in>
2020-08-30 17:54:06 +0530

    Refactor code testcaseModel.js

--- server/models/testcaseModel.js
+++ server/models/testcaseModel.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose');
 
+const subschema = new mongoose.Schema({
+  input:{
+    type:String,
+  },
+  output:{
+    type:String,
+  }
+},{ _id : false });
+
 const testcaseSchema = new mongoose.Schema({
   qid: {
     type: String,
     required: true,
   },
-  testcases: [
-    {
-        input: String,
-        output: String,
-    }
-    ]
+  testcases: {
+    type: [subschema],
+  },
 });
 
 // eslint-disable-next-line prettier/prettier
